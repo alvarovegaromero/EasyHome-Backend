447ae3ccab9817514f8271cd730175fef3ec0411
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-03 20:31:39 +0100

    login 2.0 de prueba

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.iniciar_sesion, name='inicio_sesion'),
+    path('inicio-sesion/', views.iniciar_sesion, name='inicio_sesion'),
+    path('login/', views.login_view, name='login'),
 ]
--- Backend/users/views.py
+++ Backend/users/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def iniciar_sesion(request):
+    return render(request, 'usuarios.html')
+
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -12,5 +15,7 @@ def iniciar_sesion(request):
         if user is not None:
             login(request, user)
             return render(request, 'usuarios.html', {'login_success': True})
-    return render(request, 'usuarios.html')
-
+        else:
+            return render(request, 'usuarios.html', {'login_error': True})
+    else:
+        return render(request, 'usuarios.html')
