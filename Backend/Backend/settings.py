65990f78229b5a9b8ac3f612d8d226835690d4b3
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-03 16:28:51 +0100

    MySQL added (in local_settings)

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -73,13 +73,10 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
