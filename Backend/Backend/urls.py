b6552c76b0bbebdefeaee5d699d17ef1d165ba1d
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-09 23:04:07 +0100

    Swagger added and some refactors

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('api_schema/', get_schema_view(title='API Schema'), name='api_schema'),
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('api/schema/', get_schema_view(title='API Schema'), name='api_schema'),
+    path('swagger/', TemplateView.as_view(
+        template_name='swagger.html', 
+        extra_context={'schema_url':'api_schema'}), 
+        name='swagger'),
+    path('admin/', admin.site.urls, name='admin'),
+    path('api/users/', include('users.urls'), name='users'),
 ]
--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/login', views.LoginAPIView.as_view(), name='login'),
-    path('api/logout', views.LogoutAPIView.as_view(), name='logout'),
+    path('login', views.LoginAPIView.as_view(), name='login'),
+    path('logout', views.LogoutAPIView.as_view(), name='logout'),
 ]
