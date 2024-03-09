0dbabe867fb5440340d3857485b94f18c6e0d685
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-09 22:25:00 +0100

    API schema added as URL

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
+    path('api_schema/', get_schema_view(title='API Schema'), name='api_schema'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]
