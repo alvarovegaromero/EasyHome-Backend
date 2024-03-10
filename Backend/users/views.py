8008c231c2ad77236dfcb9c8c7185306247e487e
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-10 19:05:47 +0100

    error 400 added

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -8,6 +8,10 @@ class LoginAPIView(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
+
+        if not username or not password:
+            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
