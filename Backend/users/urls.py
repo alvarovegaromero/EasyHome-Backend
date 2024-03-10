efeaf63cb8f43a6dd95329b6311b2334e5dd7d87
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-10 18:53:13 +0100

    little refactor

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import LoginAPIView, LogoutAPIView
 
 urlpatterns = [
-    path('login', views.LoginAPIView.as_view(), name='login'),
-    path('logout', views.LogoutAPIView.as_view(), name='logout'),
+    path('login', LoginAPIView.as_view(), name='login'),
+    path('logout', LogoutAPIView.as_view(), name='logout'),
 ]
