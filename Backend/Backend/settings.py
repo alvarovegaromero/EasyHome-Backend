71e4aeefb92be89b310c49a663016a3886b18d89
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-24 19:43:21 +0100

    Added hotspot wifi (Using mobile as wifi)

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-isfu*b)9-(-sy88+dr-yhngc7j!lwy(jsv0=8nucywugfxojez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '192.168.1.50']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '192.168.1.50', '192.168.78.213']
 
 # Application definition
 
