2fc3d85262ac549743a15fadb42ae30a4891f87d
Álvaro <vegaromeroalvaro@gmail.com>
2024-03-28 17:36:57 +0100

    added error code 500

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,3 +1,4 @@
+from venv import logger
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,53 +9,66 @@ from django.core.validators import validate_email
 
 class LoginAPIView(APIView):
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        try:
+            username = request.data.get('username')
+            password = request.data.get('password')
 
-        if not username or not password:
-            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+            if not username or not password:
+                return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Invalid credentials. Please, try again'}, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            logger.error("An error occurred during log in: %s" % str(e))
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class LogoutAPIView(APIView):
     def post(self, request):
-        if request.user.is_authenticated:
-            # Delete the token when the user logout - Safer but costly. 
-            # request.user.auth_token.delete() 
-            logout(request)
-            return Response({'success': 'Logout successful'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            if request.user.is_authenticated:
+                # Delete the token when the user logout - Safer but costly. 
+                # request.user.auth_token.delete() 
+                logout(request)
+                return Response({'success': 'Logout successful'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'User is not authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            logger.error("An error occurred during log out: %s" % str(e))
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class RegisterAPIView(APIView):
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        email = request.data.get('email')
-        first_name = request.data.get('firstName', '')
-        last_name = request.data.get('lastName', '')
-
-        # Validations:
-        if not username or not password or not email:
-            return Response({'error': 'Username, password, and email are required'}, status=status.HTTP_400_BAD_REQUEST)
-        if User.objects.filter(username=username).exists():
-            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        if User.objects.filter(email=email).exists():
-            return Response({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        if password != request.data.get('confirmPassword'):
-            return Response({'error': 'Passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
-        
         try:
-            validate_email(email)
-        except:
-            return Response({'error': 'Invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+            username = request.data.get('username')
+            password = request.data.get('password')
+            email = request.data.get('email')
+            first_name = request.data.get('firstName', '')
+            last_name = request.data.get('lastName', '')
+
+            # Validations:
+            if not username or not password or not email:
+                return Response({'error': 'Username, password, and email are required'}, status=status.HTTP_400_BAD_REQUEST)
+            if User.objects.filter(username=username).exists():
+                return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+            if User.objects.filter(email=email).exists():
+                return Response({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+            if password != request.data.get('confirmPassword'):
+                return Response({'error': 'Passwords do not match'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                validate_email(email)
+            except:
+                return Response({'error': 'Invalid email'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # Processing:
+            user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         
-        # Processing:
-        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logger.error("An error occurred during user registration: %s" % str(e))
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
