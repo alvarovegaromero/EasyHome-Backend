3f541f818a9f4287e640284250c23e6db70e049a
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-07 22:28:20 +0200

    New model added. Saves User + Reset Password Token

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserTokenResetPassword(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    reset_password_token = models.CharField(max_length=100, blank=True, null=True)
+
+    # TO DO: Add token expiration
+    #
+    # token_expiration = models.DateTimeField(blank=True, null=True)
+    # def is_token_valid(self):
+    #    return self.token_expiration > timezone.now()
