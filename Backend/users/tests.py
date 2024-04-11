9ec411a785c98a2709385b5ea7c5f779ed47f4fa
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-11 22:08:50 +0200

    Adding some unit tests to LoginAPIViewTest

--- Backend/users/tests.py
+++ Backend/users/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from .views import LoginAPIView
 
-# Create your tests here.
+
+class LoginAPIViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@test.com', password='testpassword')
+        self.view = LoginAPIView.as_view()
+
+    def test_login_success(self):
+        request = self.factory.post('/login/', {'username': 'testuser', 'password': 'testpassword'})
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['username'], 'testusser')
+        self.assertTrue('token' in response.data)
