c1d8ff492a7a24e6bd91e31a83c587561be71cfa
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-15 21:37:02 +0200

    Unit tests added for view: LoginAPIView

--- Backend/users/tests.py
+++ Backend/users/tests.py
@@ -1,14 +1,11 @@
 from django.test import TestCase, RequestFactory
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from .views import LoginAPIView
-
 
 class LoginAPIViewTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='testuser', email='testuser@test.com', password='testpassword')
+        self.user = User.objects.create_user(username='testuser', email='testuser@test.com', password='testpassword')
         self.view = LoginAPIView.as_view()
 
     def test_login_success(self):
@@ -16,5 +13,35 @@ class LoginAPIViewTest(TestCase):
         request.user = self.user
         response = self.view(request)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['username'], 'testusser')
+        self.assertEqual(response.data['username'], 'testuser')
         self.assertTrue('token' in response.data)
+
+    def test_login_no_username(self):
+        request = self.factory.post('/login/', {'password': 'testpassword'})
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_login_no_password(self):
+        request = self.factory.post('/login/', {'username': 'testuser'})
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_login_no_username_no_password(self):
+        request = self.factory.post('/login/', {})
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_login_wrong_password(self):
+        request = self.factory.post('/login/', {'username': 'testuser', 'password': 'wrongpassword'})
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 401)   
+
+    def test_login_non_existent_user(self):
+        request = self.factory.post('/login/', {'username': 'nonexistentuser', 'password': 'testpassword'})
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 401)
