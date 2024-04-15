ac5085b86e6a2688808f767234c5d446fccc28e9
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-15 21:59:56 +0200

    Fix name

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -5,7 +5,7 @@ from .views import render_reset_password_page
 urlpatterns = [
     path('login', LoginAPIView.as_view(), name='login'),
     path('logout', LogoutAPIView.as_view(), name='logout'),
-    path('register', RegisterAPIView.as_view(), name='logout'),
+    path('register', RegisterAPIView.as_view(), name='register'),
     path('profile', ProfileAPIView.as_view(), name='profile'),
     path('reset-password', ResetPasswordAPIView.as_view(), name='reset-password'),
     path('reset-password-form/', render_reset_password_page, name='reset-password-form'), #Maybe cool to have with a separate url
