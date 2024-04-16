0485b4a1c38a75b48f11ca20c7d73f17d97645f9
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-16 23:07:54 +0200

    Tests fixed

--- Backend/users/tests/reset_password/test_reset_password_request_view.py
+++ Backend/users/tests/reset_password/test_reset_password_request_view.py
@@ -11,7 +11,7 @@ class ResetPasswordRequestAPIViewTest(TestCase):
         self.user = User.objects.create_user(username='testuser', email='testuser@test.com', password='testpassword')
         self.url = '/api/users/reset-password-request'
 
-    @patch('users.views.reset_password_with_token')
+    @patch('users.views.reset_password.reset_password_request_view.reset_password_with_token')
     def test_post_reset_password_valid_token_and_matching_passwords(self, mock_reset_password_with_token):
         mock_reset_password_with_token.return_value = (True, 'Password reset successfully')
         response = self.client.post(self.url, {'token': 'validtoken', 'new_password': 'newpassword', 'confirm_password': 'newpassword'}, format='json')
@@ -23,7 +23,7 @@ class ResetPasswordRequestAPIViewTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['error'], 'Passwords do not match')
 
-    @patch('users.views.reset_password_with_token')
+    @patch('users.views.reset_password.reset_password_request_view.reset_password_with_token')
     def test_post_reset_password_invalid_token(self, mock_reset_password_with_token):
         mock_reset_password_with_token.return_value = (False, 'Invalid token')
         response = self.client.post(self.url, {'token': 'invalidtoken', 'new_password': 'newpassword', 'confirm_password': 'newpassword'}, format='json')
