6dc62ee791818e1dc0a8edce63324e2a7aa0a717
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-21 22:26:45 +0200

    Add user_id as part of the url for getting its groups

--- Backend/groups/urls.py
+++ Backend/groups/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views.groups_view import GroupsAPIView
 
 urlpatterns = [
-    path('groups', GroupsAPIView.as_view(), name='groups'),
+    path('<str:user_id>/groups', GroupsAPIView.as_view(), name='user_groups'),
 ]
