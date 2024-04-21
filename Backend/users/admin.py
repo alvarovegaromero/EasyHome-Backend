b16212fca3cb2cbcab2d85a3fc5ade0e38214ec6
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-21 22:27:23 +0200

    Add user_id in list_display (only django admin)

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('username', 'id', 'email', 'first_name', 'last_name')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin) 
