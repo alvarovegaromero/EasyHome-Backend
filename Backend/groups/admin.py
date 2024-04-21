7da493886ceeabbe89a5ed7b684646c648ae5aef
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-21 22:52:30 +0200

    Delete app (Broke it with migrations xd)

--- Backend/groups/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-from .models import Group, UserGroup
-
-admin.site.register(Group)
-admin.site.register(UserGroup)
--- Backend/groups/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views.groups_view import GroupsAPIView
-
-urlpatterns = [
-    path('<str:user_id>/groups', GroupsAPIView.as_view(), name='user_groups'),
-]
