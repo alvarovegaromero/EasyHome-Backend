c35c7c6a6c9735f05aa7d5aedb6877de65e2a74a
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-21 23:01:33 +0200

    Update admin.py

--- Backend/groups/admin.py
+++ Backend/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Group, UserGroup
+
+admin.site.register(Group)
+admin.site.register(UserGroup)
