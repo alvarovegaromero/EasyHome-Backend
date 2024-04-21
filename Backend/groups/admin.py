aaafed013686a7734a26e03cf8764760045348c4
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-21 23:22:42 +0200

    Refactor and added id and owner name to django admin tab

--- Backend/groups/admin.py
+++ Backend/groups/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
 from .models import Group, UserGroup
 
-admin.site.register(Group)
-admin.site.register(UserGroup)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'id', 'get_owner_name')
+
+    def get_owner_name(self, obj):
+        return obj.owner.username
+    get_owner_name.short_description = 'Owner Name' 
+
+class UserGroupAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user_id', 'group_id')
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(UserGroup, UserGroupAdmin)
