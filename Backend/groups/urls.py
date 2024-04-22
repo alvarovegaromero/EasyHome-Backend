3fef70fff101c171587bc24b19c39826ba7fbd34
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-22 19:47:25 +0200

    Added url for CreateGroupView

--- Backend/groups/urls.py
+++ Backend/groups/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.groups_view import GroupsAPIView
+from .views.groups_view import GroupCreateAPIView
 
 urlpatterns = [
     path('<str:user_id>/groups', GroupsAPIView.as_view(), name='user_groups'),
+    path('groups/create', GroupCreateAPIView.as_view(), name='create_group'),
 ]
