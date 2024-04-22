470ec4d57befbd24996a59c873e8c744ea50accf
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-22 22:15:55 +0200

    New view added in URL and little fix

--- Backend/groups/urls.py
+++ Backend/groups/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views.groups_view import GroupsAPIView
 from .views.create_group_view import GroupCreateAPIView
+from .views.group_view import GroupAPIView
 
 urlpatterns = [
-    path('<str:user_id>/groups', GroupsAPIView.as_view(), name='user_groups'),
+    path('<str:user_id>/groups', GroupsAPIView.as_view(), name='groups'),
     path('create', GroupCreateAPIView.as_view(), name='create_group'),
+    path('<str:user_id>', GroupAPIView.as_view(), name='group'),
 ]
