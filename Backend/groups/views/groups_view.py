d4c82daaafe0929629fe5724da1a685e66f0070d
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-22 19:43:44 +0200

    View added for getting all the groups for a user

--- Backend/groups/views/groups_view.py
+++ Backend/groups/views/groups_view.py
@@ -2,24 +2,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound, PermissionDenied
+from groups.models import UserGroup
 
 class GroupsAPIView(APIView):
-    def get(self, request, user_id):
-        print('User ID:', user_id)
-        
+    permission_classes = (IsAuthenticated,) #Error 403
+
+    def get_object(self, user_id):
         try:
-            user = User.objects.get(pk=user_id)
+            return User.objects.get(pk=user_id)
         except User.DoesNotExist:
-            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND) 
+            raise NotFound('A user with this ID does not exist.') #Error 404
+
+    def get(self, request, user_id):
+        user = self.get_object(user_id)
+
+        if request.user != user:
+            raise PermissionDenied('You do not have permission to access this user\'s groups.') #Error 403
+
+        user_groups = UserGroup.objects.filter(user=user)  # get all UserGroup objects user
+        group_data = [{'group_id': user_group.group.id, 
+                        'group_name': user_group.group.name, 
+                        'group_owner': user_group.group.owner.username}
+                        for user_group in user_groups
+                    ]  
+        return Response({'groups': group_data}, status=status.HTTP_200_OK)
         
-        print('User name:', user.username)
-
-        data = {
-            'message': 'Hello from GroupsAPIView!',
-            'data': {
-                'group_id': 1,
-                'group_name': 'Dummy Group'
-            }
-        }
-
-        return Response(data)
+        
