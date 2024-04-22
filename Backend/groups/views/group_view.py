38095fc531adc3b4b4f50c24c72c54acaf75ab46
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-22 22:29:30 +0200

    View created for info of the group and deleting it

---
+++ Backend/groups/views/group_view.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404
+from groups.models import Group
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class GroupAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, group_id):
+        group = get_object_or_404(Group, pk=group_id)
+        group_data = {
+            'id': group.id,
+            'name': group.name,
+            'description': group.description,
+            'currency': group.currency,
+            'creation_date': group.creation_date,
+            'owner': group.owner.username,
+        }
+        
+        return Response(group_data, status=status.HTTP_200_OK)
+
+
+    def post(self, request, group_id):
+        group = get_object_or_404(Group, pk=group_id)
+
+        if request.user != group.owner:
+            return Response({'error': 'You do not have permission to delete this group.'}, status=status.HTTP_403_FORBIDDEN)
+
+        group.delete()
+
+        return Response({'success': 'Group deleted successfully'}, status=status.HTTP_200_OK)
+        
