f068b84f12de103f7618e8d6ac01322421602d44
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-23 22:26:26 +0200

    Create get_join_code_view.py

---
+++ Backend/groups/views/get_join_code_view.py
@@ -0,0 +1,25 @@
+from django.utils import timezone
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from groups.models import Group, UserGroup
+
+
+class GroupGenerateCodeAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, group_id):
+        group = get_object_or_404(Group, pk=group_id)
+
+        if not UserGroup.objects.filter(user=request.user, group=group).exists():
+            return Response({'error': 'You are not a member of this group.'}, status=status.HTTP_403_FORBIDDEN)
+
+        if group.join_code and group.join_code_expiration > timezone.now():
+            join_code = group.join_code
+        else:
+            join_code = group.generate_join_code()
+
+        return Response({'join_code': join_code}, status=status.HTTP_200_OK)
+    
