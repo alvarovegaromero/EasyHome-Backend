1c24ed19b5fcb30803c71ebf78de6339814f3f1b
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-23 22:48:26 +0200

    reorder for avoiding bad matches

--- Backend/groups/urls.py
+++ Backend/groups/urls.py
@@ -6,11 +6,12 @@ from .views.leave_group_view import GroupLeaveAPIView
 from .views.join_group_view import GroupJoinAPIView
 from .views.get_join_code_view import GroupGenerateCodeAPIView
 
+
 urlpatterns = [
-    path('<str:user_id>/groups', GroupsAPIView.as_view(), name='groups'),
     path('create', GroupCreateAPIView.as_view(), name='create_group'),
-    path('<str:group_id>', GroupAPIView.as_view(), name='group'),
-    path('<str:group_id>/leave', GroupLeaveAPIView.as_view(), name='leave_group'),
     path('join', GroupJoinAPIView.as_view(), name='join_group'),
+    path('<str:user_id>/groups', GroupsAPIView.as_view(), name='groups'),
+    path('<str:group_id>/leave', GroupLeaveAPIView.as_view(), name='leave_group'),
     path('<str:group_id>/generate_code', GroupGenerateCodeAPIView.as_view(), name='generate_code'),
+    path('<str:group_id>', GroupAPIView.as_view(), name='group'),
 ]
