413d12d2aa036652660beb022da9251fbad69a36
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-24 20:10:53 +0200

    add try/except exception in view

--- Backend/groups/views/create_group_view.py
+++ Backend/groups/views/create_group_view.py
@@ -16,14 +16,26 @@ class GroupCreateAPIView(APIView):
 
         if not name or not currency:
             return Response({'error': 'Name and currency are required'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        with transaction.atomic():
-            group = Group.objects.create(
-                name=name,
-                description=description,
-                currency=currency,
-                owner=request.user,
-            )
 
-        #Send also group's information
-        return Response({'success': 'Group created successfully'}, status=status.HTTP_201_CREATED) 
+        try:        
+            with transaction.atomic():
+                group = Group.objects.create(
+                    name=name,
+                    description=description,
+                    currency=currency,
+                    owner=request.user,
+                )
+
+                group_data = {
+                    'id': group.id,
+                    'name': group.name,
+                    'description': group.description,
+                    'currency': group.currency,
+                    'creation_date': group.creation_date,
+                    'owner': group.owner.username,
+                }
+
+            return Response(group_data, status=status.HTTP_201_CREATED) 
+        
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
