36939ef8a8fb92d3a4d40323ccfa3e3925c89746
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-24 20:27:19 +0200

    Tests created for group view

---
+++ Backend/groups/tests/test_create_group_view.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+
+class GroupCreateAPIViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', email='testuser@test.com', password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+        self.url = '/api/groups/create'
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_create_group_authenticated(self):
+        response = self.client.post(self.url, {'name': 'Test Group', 'description': 'Test Description', 'currency': 'EUR'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Test Group')
+        self.assertEqual(response.data['currency'], 'EUR')
+        self.assertEqual(response.data['description'], 'Test Description')
+
+    def test_create_group_authenticated_no_description(self):
+        response = self.client.post(self.url, {'name': 'Test Group', 'currency': 'EUR'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Test Group')
+        self.assertEqual(response.data['currency'], 'EUR')
+    
+    def test_create_group_unauthenticated(self):
+        self.client.credentials()
+        response = self.client.post(self.url, {'name': 'Test Group', 'description': 'Test Description', 'currency': 'EUR'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        print(response.data)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_create_group_no_name(self):
+        response = self.client.post(self.url, {'description': 'Test Description', 'currency': 'EUR'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['error'], 'Name and currency are required')
+    
+    def test_create_group_no_currency(self):
+        response = self.client.post(self.url, {'name': 'Test Group', 'description': 'Test Description'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['error'], 'Name and currency are required')
