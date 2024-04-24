e7692009e6a9e828268de4a9d540e849275c1e37
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-24 22:17:15 +0200

    Update join_group_view.py

--- Backend/groups/views/join_group_view.py
+++ Backend/groups/views/join_group_view.py
@@ -1,3 +1,4 @@
+from venv import logger
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -16,16 +17,22 @@ class GroupJoinAPIView(APIView):
         if not join_code:
             return Response({'error': 'Join code is required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Get the group with the provided join code that has not expired
-        groups = Group.objects.filter(join_code=join_code, join_code_expiration__gt=timezone.now())
-        group = groups.order_by('-join_code_expiration').first()
+        try:
 
-        if group is None:
-            return Response({'error': 'Invalid or expired join code'}, status=status.HTTP_400_BAD_REQUEST)
+            # Get the group with the provided join code that has not expired
+            groups = Group.objects.filter(join_code=join_code, join_code_expiration__gt=timezone.now())
+            group = groups.order_by('-join_code_expiration').first()
 
-        if UserGroup.objects.filter(user=request.user, group=group).exists():
-            return Response({'error': 'You are already a member of this group'}, status=status.HTTP_400_BAD_REQUEST)
+            if group is None:
+                return Response({'error': 'Invalid or expired join code'}, status=status.HTTP_400_BAD_REQUEST)
 
-        UserGroup.objects.create(user=request.user, group=group)
+            if UserGroup.objects.filter(user=request.user, group=group).exists():
+                return Response({'error': 'You are already a member of this group'}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({'success': 'You have joined the group successfully.'}, status=status.HTTP_200_OK)
+            UserGroup.objects.create(user=request.user, group=group)
+
+            return Response({'success': 'You have joined the group successfully.'}, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            logger.error("An error occurred during group join: %s" % str(e))
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
