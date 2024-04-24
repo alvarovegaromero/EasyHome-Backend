9793fad4f1afd2efbeb21d7cce87a7a43d42e1d3
Álvaro <vegaromeroalvaro@gmail.com>
2024-04-24 20:35:06 +0200

    Update leave_group_view.py

--- Backend/groups/views/leave_group_view.py
+++ Backend/groups/views/leave_group_view.py
@@ -1,3 +1,4 @@
+from venv import logger
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -12,16 +13,22 @@ class GroupLeaveAPIView(APIView):
         group = get_object_or_404(Group, pk=group_id)
         user_group = get_object_or_404(UserGroup, user=request.user, group=group)
 
-        if group.owner == request.user:
-            # If the user is the owner of the group, transfer ownership to the next user
-            next_owner_user_group = UserGroup.objects.filter(group=group).exclude(user=request.user).order_by('join_date').first()
+        try: 
+            if group.owner == request.user:
+                # If the user is the owner of the group, transfer ownership to the next user
+                next_owner_user_group = UserGroup.objects.filter(group=group).exclude(user=request.user).order_by('join_date').first()
 
-            if next_owner_user_group is None:
-                return Response({'error': 'You are the last member of the group. Please delete the group instead.'}, status=status.HTTP_400_BAD_REQUEST)
+                if next_owner_user_group is None:
+                    return Response({'error': 'You are the last member of the group. Please delete the group instead.'}, status=status.HTTP_400_BAD_REQUEST)
 
-            group.owner = next_owner_user_group.user
-            group.save()
+                group.owner = next_owner_user_group.user
+                group.save()
 
-        user_group.delete()
+            user_group.delete()
 
-        return Response({'success': 'You have left the group successfully.'}, status=status.HTTP_200_OK)
+            return Response({'success': 'You have left the group successfully.'}, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            logger.error("An error occurred during group left: %s" % str(e))
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
