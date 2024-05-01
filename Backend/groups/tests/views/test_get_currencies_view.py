4279b1642467049a66b70deca7eb0bc6af59cdca
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-01 22:06:44 +0200

    Test added for getCurrencies View

---
+++ Backend/groups/tests/views/test_get_currencies_view.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+from groups.currency_choices import CURRENCY_CHOICES
+from rest_framework import status
+
+class CurrenciesAPIViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_currencies(self):
+        response = self.client.get('/api/groups/currencies')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, CURRENCY_CHOICES)
