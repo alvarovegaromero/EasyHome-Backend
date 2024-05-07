babeba1f8bf83e6c5718b6fe802753d2f6b79b51
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-07 23:17:13 +0200

    added imports

--- Backend/groups/urls.py
+++ Backend/groups/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views.groups_view import GroupsAPIView
 from .views.create_group_view import GroupCreateAPIView
 from .views.group_view import GroupAPIView
@@ -6,6 +7,8 @@ from .views.leave_group_view import GroupLeaveAPIView
 from .views.join_group_view import GroupJoinAPIView
 from .views.get_join_code_view import GroupGenerateCodeAPIView
 from .views.get_currencies_view import CurrenciesAPIView
+from .views.change_owner_view import GroupChangeOwnerAPIView
+from .views.kick_user_view import GroupKickUserAPIView
 
 
 urlpatterns = [
@@ -16,6 +19,6 @@ urlpatterns = [
     path('<str:group_id>/leave', GroupLeaveAPIView.as_view(), name='leave_group'),
     path('<str:group_id>/generate_code', GroupGenerateCodeAPIView.as_view(), name='generate_code'),
     path('<str:group_id>', GroupAPIView.as_view(), name='group'),
-    #path('<str:group_id>/kick/<str:user_id>', GroupKickUserAPIView.as_view(), name='kick_user'),
-    #path('<str:group_id>/change_owner/<str:user_id>', GroupChangeOwnerAPIView.as_view(), name='change_owner'),
+    path('<str:group_id>/kick/<str:user_id>', GroupKickUserAPIView.as_view(), name='kick_user'),
+    path('<str:group_id>/change_owner/<str:user_id>', GroupChangeOwnerAPIView.as_view(), name='change_owner'),
 ]
