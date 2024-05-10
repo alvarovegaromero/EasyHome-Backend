88609c3e082e85ab3851387c1b5cf32ba891f2dd
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-10 16:48:48 +0200

    Fix views. Encapsulating them in try/except code in case error code 500 arise

--- Backend/groups/views/change_owner_view.py
+++ Backend/groups/views/change_owner_view.py
@@ -4,29 +4,34 @@ from rest_framework.response import Response
 from groups.models import Group, UserGroup
 from rest_framework import status
 from django.db import transaction
+from venv import logger
 
 
 class GroupChangeOwnerAPIView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, group_id, user_id):
-
         try:
-            group = Group.objects.get(id=group_id)
-        except Group.DoesNotExist:
-            return Response({'error': "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+            try:
+                group = Group.objects.get(id=group_id)
+            except Group.DoesNotExist:
+                return Response({'error': "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+            
+            if not UserGroup.objects.filter(user_id=user_id, group_id=group_id).exists():
+                return Response({'error': "User is not a member of this group"}, status=status.HTTP_404_NOT_FOUND)
+            
+            if request.user != group.owner:
+                return Response({'error': "Only the group owner can change ownership"}, status=status.HTTP_403_FORBIDDEN)
+
+            if str(request.user.id) == user_id:
+                return Response({'error': "You are already the owner of the group"}, status=status.HTTP_400_BAD_REQUEST)
+
+            with transaction.atomic():
+                group.owner = user_id
+                group.save()
+
+            return Response({'success': "Ownership of the group has been changed succesfully"}, status=status.HTTP_200_OK)
         
-        if not UserGroup.objects.filter(user_id=user_id, group_id=group_id).exists():
-            return Response({'error': "User is not a member of this group"}, status=status.HTTP_404_NOT_FOUND)
-        
-        if request.user != group.owner:
-            return Response({'error': "Only the group owner can change ownership"}, status=status.HTTP_403_FORBIDDEN)
-
-        if request.user.id == user_id:
-            return Response({'error': "You are already the owner of the group"}, status=status.HTTP_400_BAD_REQUEST)
-
-        with transaction.atomic():
-            group.owner = user_id
-            group.save()
-
-        return Response({'success': "Ownership of the group has been changed succesfully"}, status=status.HTTP_200_OK)
+        except Exception as e:
+                logger.error("An error occurred during group join: %s" % str(e))
+                return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- Backend/groups/views/kick_user_view.py
+++ Backend/groups/views/kick_user_view.py
@@ -4,31 +4,35 @@ from groups.models import Group, UserGroup
 from rest_framework import status
 from rest_framework.response import Response
 from django.db import transaction
+from venv import logger
 
 
 class GroupKickUserAPIView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, group_id, user_id):
-
         try:
-            group = Group.objects.get(id=group_id)
-        except Group.DoesNotExist:
-            return Response({'error': "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+            try:
+                group = Group.objects.get(id=group_id)
+            except Group.DoesNotExist:
+                return Response({'error': "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
 
-        try:
-            user_group = UserGroup.objects.get(user_id=user_id, group_id=group_id)
-        except UserGroup.DoesNotExist:
-            return Response({'error': "User is not a member of this group"} , status=status.HTTP_404_NOT_FOUND)
+            try:
+                user_group = UserGroup.objects.get(user_id=user_id, group_id=group_id)
+            except UserGroup.DoesNotExist:
+                return Response({'error': "User is not a member of this group"} , status=status.HTTP_404_NOT_FOUND)
 
-        if request.user != group.owner:
-            return Response({'error': "Only the group owner can kick users"} , status=status.HTTP_403_FORBIDDEN)
+            if request.user != group.owner:
+                return Response({'error': "Only the group owner can kick users"} , status=status.HTTP_403_FORBIDDEN)
 
-        if request.user.id == user_id:
-            return Response({'error': "You can not kick yourself. Leave the group instead"} , status=status.HTTP_400_BAD_REQUEST)
+            if str(request.user.id) == user_id:
+                return Response({'error': "You can not kick yourself. Leave the group instead"} , status=status.HTTP_400_BAD_REQUEST)
         
-        with transaction.atomic():
-            user_group.delete()
+            with transaction.atomic():
+                user_group.delete()
 
-        return Response({'success': "User has been succesfully kicked from the group"}, status=status.HTTP_200_OK)
-    
+            return Response({'success': "User has been succesfully kicked from the group"}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            logger.error("An error occurred during group join: %s" % str(e))
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
