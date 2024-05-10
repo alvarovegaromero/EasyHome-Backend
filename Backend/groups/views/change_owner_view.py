aa9b92656b78b44ca938c577a261c16e39174851
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-10 21:57:33 +0200

    Check if user exists and some fixes

--- Backend/groups/views/change_owner_view.py
+++ Backend/groups/views/change_owner_view.py
@@ -5,6 +5,7 @@ from groups.models import Group, UserGroup
 from rest_framework import status
 from django.db import transaction
 from venv import logger
+from django.contrib.auth.models import User
 
 
 class GroupChangeOwnerAPIView(APIView):
@@ -16,7 +17,12 @@ class GroupChangeOwnerAPIView(APIView):
                 group = Group.objects.get(id=group_id)
             except Group.DoesNotExist:
                 return Response({'error': "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
-            
+
+            try:
+                new_owner = User.objects.get(id=user_id)
+            except User.DoesNotExist:
+                return Response({'error': "User wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+
             if not UserGroup.objects.filter(user_id=user_id, group_id=group_id).exists():
                 return Response({'error': "User is not a member of this group"}, status=status.HTTP_404_NOT_FOUND)
             
@@ -27,11 +33,11 @@ class GroupChangeOwnerAPIView(APIView):
                 return Response({'error': "You are already the owner of the group"}, status=status.HTTP_400_BAD_REQUEST)
 
             with transaction.atomic():
-                group.owner = user_id
+                group.owner = new_owner
                 group.save()
 
             return Response({'success': "Ownership of the group has been changed succesfully"}, status=status.HTTP_200_OK)
         
         except Exception as e:
-                logger.error("An error occurred during group join: %s" % str(e))
+                logger.error("An error occurred during ownership change: %s" % str(e))
                 return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- Backend/groups/views/kick_user_view.py
+++ Backend/groups/views/kick_user_view.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.db import transaction
 from venv import logger
+from django.contrib.auth.models import User
 
 
 class GroupKickUserAPIView(APIView):
@@ -17,6 +18,9 @@ class GroupKickUserAPIView(APIView):
             except Group.DoesNotExist:
                 return Response({'error': "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
 
+            if not User.objects.filter(id=user_id).exists():
+                return Response({'error': "User wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+            
             try:
                 user_group = UserGroup.objects.get(user_id=user_id, group_id=group_id)
             except UserGroup.DoesNotExist:
@@ -34,5 +38,5 @@ class GroupKickUserAPIView(APIView):
             return Response({'success': "User has been succesfully kicked from the group"}, status=status.HTTP_200_OK)
 
         except Exception as e:
-            logger.error("An error occurred during group join: %s" % str(e))
-            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            logger.error("An error occurred during expulsion: %s" % str(e))
+            return Response({'error': "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
