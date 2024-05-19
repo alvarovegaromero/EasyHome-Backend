60c33fe629acef03fb1e0dbcc3f7f4a5930edb86
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-19 18:23:31 +0200

    Added in admin

--- Backend/shared_board/admin.py
+++ Backend/shared_board/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import SharedBoard
 
 # Register your models here.
+
+class SharedBoardAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'get_group_name', 'get_group_id')
+
+    def get_group_name(self, obj):
+        return obj.group.name
+    get_group_name.short_description = 'Group Name'
+
+    def get_group_id(self, obj):
+        return obj.group.id
+    get_group_id.short_description = 'Group ID'
+
+admin.site.register(SharedBoard, SharedBoardAdmin)
