9eabd4fe8b3722bec2f4369f6f8364cebd40815b
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-19 17:21:05 +0200

    view added for get

--- Backend/shared_board/views/shared_board_view.py
+++ Backend/shared_board/views/shared_board_view.py
@@ -1,22 +1,33 @@
 from venv import logger
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from groups.models import Group, UserGroup
+from shared_board.models import SharedBoard
 
 class SharedBoardView(APIView):
     permission_classes = (IsAuthenticated,) 
 
-    def post(self, request):
+    def post(self, request, group_id):
         try:
             pass
         except Exception as e:
             logger.error("An error occurred during shared board edition: %s" % str(e))
             return Response({'error': "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def get(self, request):
+    def get(self, request, group_id):
         try:
-            pass
+            group = get_object_or_404(Group, pk=group_id)
+
+            if not UserGroup.objects.filter(user=request.user, group=group).exists():
+                return Response({'error': 'You do not belong to this group.'}, status=status.HTTP_403_FORBIDDEN)                
+                
+            board = SharedBoard.objects.get(group=group)
+            request.session['last_edited'] = str(board.last_edited)
+            return ({'content': board.content})
+
         except Exception as e:
             logger.error("An error occurred during shared board retrieval: %s" % str(e))
             return Response({'error': "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
