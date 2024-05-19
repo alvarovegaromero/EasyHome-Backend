cc795b4a3a89c5c9f214927a4ffb57574e6446c3
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-19 16:42:40 +0200

    Model created for SharedBoard

--- Backend/shared_board/models.py
+++ Backend/shared_board/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
-# Create your models here.
+class SharedBoard(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    last_edited = models.DateTimeField(auto_now=True)
