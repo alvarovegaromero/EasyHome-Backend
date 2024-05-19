86ab97063ae44638bcc98c824e6ba6ef6bd1f213
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-19 15:50:05 +0200

    Prepare code for the new views

---
+++ Backend/shared_board/views/shared_board_view.py
@@ -0,0 +1,22 @@
+from venv import logger
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+class SharedBoard(APIView):
+    permission_classes = (IsAuthenticated,) 
+
+    def post(self, request):
+        try:
+            pass
+        except Exception as e:
+            logger.error("An error occurred during shared board edition: %s" % str(e))
+            return Response({'error': "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def get(self, request):
+        try:
+            pass
+        except Exception as e:
+            logger.error("An error occurred during shared board retrieval: %s" % str(e))
+            return Response({'error': "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
