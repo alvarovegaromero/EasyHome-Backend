e6dc7115a1e6755a419c976292ed59ff062c2217
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-28 20:01:50 +0200

    Model added and migrated

--- Backend/expense_distribution/models.py
+++ Backend/expense_distribution/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Expense(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    group = models.OneToOneField("groups.Group", on_delete=models.CASCADE)
+    paid_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="paid_expenses")
+    debtors = models.ManyToManyField(User, related_name="debtors")
+    date_added = models.DateTimeField(auto_now_add=True)  # can't be changed
+    date_paid = models.DateTimeField(default=timezone.now)  # can be changed in the future
