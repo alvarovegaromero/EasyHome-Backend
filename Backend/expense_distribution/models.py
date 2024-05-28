5a7a2de32c4c521e8f71a3e42613c4e0caedf3ed
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-28 23:49:45 +0200

    Fix expenses model - Allow group having multiple expenses

--- Backend/expense_distribution/models.py
+++ Backend/expense_distribution/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Expense(models.Model):
     name = models.CharField(max_length=50)
     amount = models.DecimalField(max_digits=7, decimal_places=2)
-    group = models.OneToOneField("groups.Group", on_delete=models.CASCADE)
+    group = models.ForeignKey("groups.Group", on_delete=models.CASCADE, related_name="expenses")
     paid_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="paid_expenses")
     debtors = models.ManyToManyField(User, related_name="debtors")
     date_added = models.DateTimeField(auto_now_add=True)  # can't be changed
