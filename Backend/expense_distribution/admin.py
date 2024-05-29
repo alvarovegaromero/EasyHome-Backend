38c5c5c21a7d2fddaa00ba952a5fb859f9207fbf
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-29 10:38:34 +0200

    Added correct columns in django admin

--- Backend/expense_distribution/admin.py
+++ Backend/expense_distribution/admin.py
@@ -1,3 +1,22 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Expense
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_group_name", "get_group_id")
+
+    def get_group_name(self, obj):
+        return obj.group.name
+
+    get_group_name.short_description = "Shared Board Group Name"
+
+    def get_group_id(self, obj):
+        return obj.group.id
+
+    get_group_id.short_description = "Group ID"
+
+
+admin.site.register(Expense, ExpenseAdmin)
+
+# try to choose only among group members once the group is selected
