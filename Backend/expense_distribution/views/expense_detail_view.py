f372f5d019465a2f969640544a56888cf92af9e8
Álvaro <vegaromeroalvaro@gmail.com>
2024-05-31 20:02:21 +0200

    Add view for expense deletion

--- Backend/expense_distribution/views/expense_detail_view.py
+++ Backend/expense_distribution/views/expense_detail_view.py
@@ -39,3 +39,32 @@ class ExpenseDetailView(APIView):
                 {"error": "Internal Server Error"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+    def delete(self, request, group_id, expense_id):
+        try:
+            try:
+                group = Group.objects.get(id=group_id)
+            except Group.DoesNotExist:
+                return Response({"error": "Group wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+
+            if not UserGroup.is_member(request.user, group):
+                return Response(
+                    {"error": "You are not a member of this group"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+
+            try:
+                expense = Expense.objects.get(id=expense_id, group=group)
+            except Expense.DoesNotExist:
+                return Response({"error": "Expense wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+
+            expense.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except Exception as e:
+            logger.error("An error occurred during expense deletion: %s" % str(e))
+            return Response(
+                {"error": "Internal Server Error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
