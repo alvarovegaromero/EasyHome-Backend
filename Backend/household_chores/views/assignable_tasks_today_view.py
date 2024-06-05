f31dfe3d35993fb7f42f0dd12ab181ca7f86ea60
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-06 00:23:18 +0200

    redirect to range view in today's view

--- Backend/household_chores/views/assignable_tasks_today_view.py
+++ Backend/household_chores/views/assignable_tasks_today_view.py
@@ -1,3 +1,7 @@
+from datetime import date
+
+from django.shortcuts import redirect
+from django.urls import reverse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
@@ -8,4 +12,7 @@ class AssignableTasksTodayAPIView(APIView):
     permission_classes = (IsAuthenticated, IsGroupMember)
 
     def get(self, request, group_id):
-        pass
+        today = date.today()
+        url = reverse("assignable_task_range", args=[group_id])
+        url += f"?start_date={today}&end_date={today}"
+        return redirect(url)
