3ae36b9857a4374ffc2f2a70d3763eae06052623
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-05 18:41:01 +0200

    little fix in expenses' endpoints

--- Backend/expense_distribution/urls.py
+++ Backend/expense_distribution/urls.py
@@ -5,11 +5,11 @@ from .views.group_expenses_view import GroupExpensesView
 from .views.group_settlement_view import GroupSettlementView
 
 urlpatterns = [
-    path("<int:group_id>/expenses", GroupExpensesView.as_view(), name="group_expenses"),
+    path("<str:group_id>/expenses", GroupExpensesView.as_view(), name="group_expenses"),
     path(
-        "<int:group_id>/expenses/<int:expense_id>",
+        "<str:group_id>/expenses/<int:expense_id>",
         ExpenseDetailView.as_view(),
         name="expense_detail",
     ),
-    path("<int:group_id>/settlements", GroupSettlementView.as_view(), name="group_settlements"),
+    path("<str:group_id>/settlements", GroupSettlementView.as_view(), name="group_settlements"),
 ]
