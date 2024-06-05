587c65383024ca715440df43f1e2525581a24e91
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-05 19:28:04 +0200

    remove not needed code for tasks' views

--- Backend/household_chores/views/task_retrieve_update_delete_view.py
+++ Backend/household_chores/views/task_retrieve_update_delete_view.py
@@ -12,31 +12,22 @@ from ..permissions.is_group_owner import IsGroupOwner
 class TaskRetrieveUpdateDeleteAPIView(APIView):
     permission_classes = (IsAuthenticated, IsGroupOwner)
 
-    def get(self, request, group_id, task_id):
+    def put(self, request, group_id, task_id):
         try:
             try:
                 task = Task.objects.get(id=task_id, group_id=group_id)
             except Task.DoesNotExist:
                 return Response({"error": "Task wasn't found"}, status=status.HTTP_404_NOT_FOUND)
 
-            return Response(task.to_dict(), status=status.HTTP_200_OK)
-
+            if "title" not in request.data:
+                return Response(
+                    {"error": "The 'title' field is required."}, status=status.HTTP_400_BAD_REQUEST
+                )
         except Exception as e:
-            logger.error("An error occurred during task retrieval: %s" % str(e))
+            logger.error("An error occurred during task modification: %s" % str(e))
             return Response(
                 {"error": "Internal Server Error"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
-
-    def put(self, request, group_id, task_id):
-        try:
-            task = Task.objects.get(id=task_id, group_id=group_id)
-        except Task.DoesNotExist:
-            return Response({"error": "Task wasn't found"}, status=status.HTTP_404_NOT_FOUND)
-
-        if "title" not in request.data:
-            return Response(
-                {"error": "The 'title' field is required."}, status=status.HTTP_400_BAD_REQUEST
             )
 
         task.title = request.data["title"]
