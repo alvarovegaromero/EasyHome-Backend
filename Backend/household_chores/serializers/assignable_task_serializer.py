8092df9817d1124f778a39e5e129d41db56b6379
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-05 23:52:29 +0200

    provide username of the user also in serializer

--- Backend/household_chores/serializers/assignable_task_serializer.py
+++ Backend/household_chores/serializers/assignable_task_serializer.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import User
 from household_chores.models import AssignableTask
 from rest_framework import serializers
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
+
+
 class AssignableTaskSerializer(serializers.ModelSerializer):
+    assigned_user = UserSerializer(read_only=True)
+
     class Meta:
         model = AssignableTask
         fields = ["id", "task", "assigned_user", "is_completed", "date"]
