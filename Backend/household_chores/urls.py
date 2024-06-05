a12c09a381132cb40c93ba3460eabf7790df7ac6
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-05 23:20:10 +0200

    go back to ids as ints

--- Backend/household_chores/urls.py
+++ Backend/household_chores/urls.py
@@ -7,22 +7,22 @@ from .views.task_list_create_view import TaskListCreateAPIView
 from .views.task_retrieve_update_delete_view import TaskRetrieveUpdateDeleteAPIView
 
 urlpatterns = [
-    path("<str:group_id>/tasks", TaskListCreateAPIView.as_view(), name="tasks"),
+    path("<int:group_id>/tasks", TaskListCreateAPIView.as_view(), name="tasks"),
     path(
-        "<str:group_id>/tasks/<str:task_id>", TaskRetrieveUpdateDeleteAPIView.as_view(), name="task"
+        "<int:group_id>/tasks/<int:task_id>", TaskRetrieveUpdateDeleteAPIView.as_view(), name="task"
     ),
     path(
-        "<str:group_id>/tasks/assign/today",
+        "<int:group_id>/tasks/assign/today",
         AssignableTasksTodayAPIView.as_view(),
         name="assign_task",
     ),
     path(
-        "<str:group_id>/tasks/assign/<str:assignable_task_id>",
+        "<int:group_id>/tasks/assign/<int:assignable_task_id>",
         AssignableTaskUpdateAPIView.as_view(),
         name="complete_assignable_task",
     ),
     path(
-        "<str:group_id>/tasks/assign/",
+        "<int:group_id>/tasks/assign/range",
         AssignableTasksInRangeAPIView.as_view(),
         name="assignable_task_range",
     ),
