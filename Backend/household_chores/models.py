7cc35154c0d514e8332538e795f1cb71a93dbe61
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-05 17:12:13 +0200

    new model created and migrated

--- Backend/household_chores/models.py
+++ Backend/household_chores/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    group = models.ForeignKey("groups.Group", on_delete=models.CASCADE, related_name="tasks")
+
+
+class SelectableTask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="selectable_tasks")
+    assigned_user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_tasks"
+    )
+    is_completed = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)  # can't be changed
