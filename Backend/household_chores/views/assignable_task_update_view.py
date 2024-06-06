6dd9a31b37b417365ee4ac7ed3fb98c4cc084531
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-06 10:22:19 +0200

    Add try-except blocks

--- Backend/household_chores/views/assignable_task_update_view.py
+++ Backend/household_chores/views/assignable_task_update_view.py
@@ -1,3 +1,7 @@
+from venv import logger
+
+from requests import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
@@ -8,4 +12,11 @@ class AssignableTaskUpdateAPIView(APIView):
     permission_classes = (IsAuthenticated, IsGroupMember)
 
     def put(self, request, group_id, assignable_task_id):
-        pass
+        try:
+            pass
+        except Exception as e:
+            logger.error("An error occurred during assignable task update: %s" % str(e))
+            return Response(
+                {"error": "Internal Server Error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
--- Backend/household_chores/views/assignable_tasks_in_range_view.py
+++ Backend/household_chores/views/assignable_tasks_in_range_view.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from venv import logger
 
 from django.utils.dateparse import parse_date
 from household_chores.models import AssignableTask
@@ -15,48 +16,62 @@ class AssignableTasksInRangeAPIView(APIView):
     permission_classes = (IsAuthenticated, IsGroupMember)
 
     def get(self, request, group_id):
-        start_date = request.query_params.get("start_date")
-        end_date = request.query_params.get("end_date")
-        is_completed = request.query_params.get("is_completed")  # Optional
-        user_id = request.query_params.get("user_id")  # Optional
+        try:
+            start_date = request.query_params.get("start_date")
+            end_date = request.query_params.get("end_date")
+            is_completed = request.query_params.get("is_completed")  # Optional
+            user_id = request.query_params.get("user_id")  # Optional
 
-        if not start_date or not end_date:
-            return Response(
-                {"error": "Both 'start_date' and 'end_date' are required."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        start_date = parse_date(start_date)
-        end_date = parse_date(end_date)
-        end_date = end_date + timedelta(days=1) - timedelta(seconds=1)  # Set end_date to 23:59:59
-
-        if not start_date or not end_date:
-            return Response(
-                {"error": "Invalid date format. Use 'YYYY-MM-DD'."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        if start_date > end_date:
-            return Response(
-                {"error": "'start_date' must be before 'end_date'."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        filters = {"task__group_id": group_id, "date__range": [start_date, end_date]}
-
-        if is_completed is not None:
-            filters["is_completed"] = is_completed.lower() == "true"
-
-        if user_id is not None:
-            if is_completed is False:
+            if not start_date or not end_date:
                 return Response(
-                    {"error": "'is_completed' can not be set to false when 'user_id' is provided."},
+                    {"error": "Both 'start_date' and 'end_date' are required."},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-            filters["assigned_user_id"] = user_id
 
-        tasks = AssignableTask.objects.filter(**filters)
+            start_date = parse_date(start_date)
+            end_date = parse_date(end_date)
+            end_date = (
+                end_date + timedelta(days=1) - timedelta(seconds=1)
+            )  # Set end_date to 23:59:59
 
-        serializer = AssignableTaskSerializer(tasks, many=True)
+            if not start_date or not end_date:
+                return Response(
+                    {"error": "Invalid date format. Use 'YYYY-MM-DD'."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            if start_date > end_date:
+                return Response(
+                    {"error": "'start_date' must be before 'end_date'."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            filters = {"task__group_id": group_id, "date__range": [start_date, end_date]}
+
+            if is_completed is not None:
+                filters["is_completed"] = is_completed.lower() == "true"
+
+            if user_id is not None:
+                if is_completed is False:
+                    return Response(
+                        {
+                            "error": (
+                                "'is_completed' can not be set to false "
+                                "when 'user_id' is provided."
+                            )
+                        },
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                filters["assigned_user_id"] = user_id
+
+            tasks = AssignableTask.objects.filter(**filters)
+
+            serializer = AssignableTaskSerializer(tasks, many=True)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error("An error occurred during assignable tasks retrieval: %s" % str(e))
+            return Response(
+                {"error": "Internal Server Error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
