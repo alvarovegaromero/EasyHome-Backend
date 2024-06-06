816aba051c544c2386ada3cbba515d0038dbc534
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-06 12:09:23 +0200

    method done for creating first assignable tasks in a group

--- Backend/household_chores/views/start_assignable_task_view.py
+++ Backend/household_chores/views/start_assignable_task_view.py
@@ -1,11 +1,39 @@
+from venv import logger
+
+from household_chores.models import AssignableTask
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..permissions.is_group_owner import IsGroupOwner
+from ..serializers.assignable_task_serializer import AssignableTaskSerializer
 
 
 class StartAssignableTasksAPIView(APIView):
     permission_classes = (IsAuthenticated, IsGroupOwner)
 
     def post(self, request, group_id):
-        pass
+        try:
+            if AssignableTask.objects.filter(task__group_id=group_id).exists():
+                return Response(
+                    {"message": "Assignable tasks already started for the group"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            tasks = AssignableTask.objects.filter(task__group_id=group_id)
+
+            assignable_tasks = []
+            for task in tasks:
+                assignable_task = AssignableTask(task=task)
+                assignable_task.save()
+                assignable_tasks.append(assignable_task)
+
+            serializer = AssignableTaskSerializer(assignable_tasks, many=True)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logger.error("An error occurred during assignable tasks retrieval: %s" % str(e))
+            return Response(
+                {"error": "Internal Server Error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
