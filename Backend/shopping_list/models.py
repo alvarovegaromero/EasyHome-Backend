56121c12794a93787f15f32983bb0c39cf92cc3d
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-07 21:33:36 +0200

    Model for shopping list created and migrated

--- Backend/shopping_list/models.py
+++ Backend/shopping_list/models.py
@@ -1,3 +1,29 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+def get_current_date():
+    return timezone.now().date()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.CASCADE, related_name="group_products"
+    )
+
+
+class ProductToBuy(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="purchase_intentions"
+    )
+
+
+class ProductBought(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="purchases")
+    date = models.DateField(default=get_current_date)
+    price = models.FloatField()
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="purchased_products"
+    )
