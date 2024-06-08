9ac126ffa3b974dd593491977bd808bf8689fb04
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-08 16:33:49 +0200

    product update feature added

--- Backend/shopping_list/views/product_update_delete_view.py
+++ Backend/shopping_list/views/product_update_delete_view.py
@@ -7,12 +7,35 @@ from rest_framework.views import APIView
 from shopping_list.models import Product
 from utils.permissions.is_group_owner import IsGroupOwner
 
+from ..serializers.product_serializer import ProductSerializer
+
 
 class ProductUpdateDeleteAPIView(APIView):
     permission_classes = (IsAuthenticated, IsGroupOwner)
 
     def put(self, request, group_id, product_id):
-        pass
+        try:
+            if "name" not in request.data or request.data["name"].strip() == "":
+                return Response(
+                    {"error": "The 'name' field is required."}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+            try:
+                product = Product.objects.get(id=product_id, group_id=group_id)
+            except Product.DoesNotExist:
+                return Response({"error": "Product wasn't found"}, status=status.HTTP_404_NOT_FOUND)
+
+            product.name = request.data["name"].strip()
+            product.save()
+
+            return Response(ProductSerializer(product).data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            logger.error("An error occurred during product modification: %s" % str(e))
+            return Response(
+                {"error": "Internal Server Error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     def delete(self, request, group_id, product_id):
         try:
