79722dfa6fcaacece339ce56ee3451a032e772bb
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-08 21:10:46 +0200

    send in get products endpoint if product is marked as to buy or not

--- Backend/shopping_list/serializers/product_serializer.py
+++ Backend/shopping_list/serializers/product_serializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from shopping_list.models import Product
+from shopping_list.models import Product, ProductToBuy
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    marked_to_buy = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ["id", "name", "group"]
+        fields = ["id", "name", "group", "marked_to_buy"]
+
+    def get_marked_to_buy(self, obj):
+        return ProductToBuy.objects.filter(product=obj).exists()
