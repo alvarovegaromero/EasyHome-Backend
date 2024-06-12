e0f78a83d8d02d7c50c4108620e44d55acbc418a
Álvaro <vegaromeroalvaro@gmail.com>
2024-06-12 19:45:15 +0200

    feat: View for veryfing the user and provide feedback

--- Backend/users/views/email_verification/email_verification_view.py
+++ Backend/users/views/email_verification/email_verification_view.py
@@ -1,5 +1,19 @@
+from django.http import HttpResponse
 from rest_framework.views import APIView
+
+from ...utils.verify_email import verify_email_with_token
 
 
 class EmailVerificationAPIView(APIView):
-    pass
+    def get(self, request):
+        token = request.GET.get("token")
+        if not token:
+            return HttpResponse("<h1>Token is required</h1>", status=400)
+
+        is_verified, message = verify_email_with_token(token)
+        if is_verified:
+            return HttpResponse(
+                f"<h1>{message}</h1> <p> You can go now back to the app <p> ", status=200
+            )
+        else:
+            return HttpResponse(f"<h1>{message}</h1>", status=400)
